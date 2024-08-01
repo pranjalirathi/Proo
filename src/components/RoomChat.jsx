04e9db23f092e08f212b9a7bffb2e3e1b2fca1c8
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-08-01 09:30:15 +0530

    chat_mesgs_width

--- src/components/RoomChat.jsx
+++ src/components/RoomChat.jsx
@@ -6,7 +6,7 @@ import MembersList from './MembersList';
 
 const AvatarSkeleton = () => (
   <svg className="w-6 h-6 text-gray-500 dark:text-gray-700 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
   </svg>
 );
 
@@ -172,9 +172,10 @@ const RoomChat = ({ roomId }) => {
                 alt="Profile Pic"
                 className="w-10 h-10 rounded-full mr-4"
               />
-              <div className="flex flex-col max-w-xs">
+              <div className="flex flex-col max-w-lg min-w-[50px]">
                 <div
                   className={`p-2 rounded-lg ${msg.is_code ? 'bg-gray-800' : msg.sender_id === localStorage.getItem('user_id') ? 'bg-blue-600' : 'bg-gray-700'}`}
+                  style={{ overflowWrap: 'break-word' }}
                 >
                   {msg.is_code ? (
                     <pre className="whitespace-pre-wrap">{msg.message}</pre>
@@ -189,6 +190,7 @@ const RoomChat = ({ roomId }) => {
         ))}
       </div>
 
+      
       {/* Typing Bar */}
       <div className="flex items-center p-2 sm:p-4 border-t border-gray-700 bg-gray-800 mt-auto">
         <input
@@ -198,7 +200,7 @@ const RoomChat = ({ roomId }) => {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           onKeyUp={(e) => {
-            if ( e.key === 'Enter') {
+            if (e.key === 'Enter') {
               handleSendMessage();
             }
           }}
